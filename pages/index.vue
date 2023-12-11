8f480a1b030cb1c0385a215b301c2a3a375de829
Guigui0025 <guiguidr25@gmail.com>
2023-12-11 12:15:18 +0100

    premiers_pas

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+const query = gql`
+  query PageAccueil {
+    page(where: { slug: "accueil" }) {
+      titre
+      slug
+      texte {
+        html
+      }
+    }
+  }
+`;
+
+const contenuAccueil = ref();
+const { data } = await useAsyncQuery(query);
+console.log(data.value);
+contenuAccueil.value = data.value.page;
+</script>
+
+<template>
+  <nav>
+
+    
+  </nav>>
+  <div v-html="contenuAccueil.texte.html"></div>
+</template>
